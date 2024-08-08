64a4d8eea496bc302e212e55b061342d2e39c595
kimjunbnbbbbin <rlawnsqls397@naver.com>
2024-08-08 12:55:41 +0900

    Add Weather Api V1.0

--- app/src/main/java/com/example/spap/Weather/ModelWeatherData.kt
+++ app/src/main/java/com/example/spap/Weather/ModelWeatherData.kt
@@ -1,3 +1,8 @@
 package com.example.spap.Weather
 
-data class ModelWeatherData()
+data class WEATHER (val response : RESPONSE)
+data class RESPONSE(val header : HEADER, val body : BODY)
+data class HEADER(val resultCode : Int, val resultMsg : String)
+data class BODY(val dataType : String, val items : ITEMS, val totalCount : Int)
+data class ITEMS(val item : List<ITEM>)
+data class ITEM(val category : String, val fcstDate : String, val fcstTime : String, val fcstValue : String)
